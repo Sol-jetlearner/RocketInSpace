25ce6e6f1f3ec3e2670ab1431649acb30a210a77
Sol-jetlearner <soleymanharboun@gmail.com>
2026-05-30 10:44:36 +0200

    Add player movement and background rendering
    Implement basic player movement controls using arrow keys.

---
+++ main.py
@@ -0,0 +1,57 @@
+#pygame.transform.scale(rocket,(200,400))
+import pygame
+from pygame.locals import *
+from time import *
+screen = pygame.display.set_mode((800,800))
+player_x = 400
+player_y = 400
+keys = [False,False,False,False]
+
+player = pygame.image.load("RocketInSpace/rocket.png")
+background = pygame.image.load("RocketInSpace\spacebg.png")
+
+while player_y < 800:
+    screen.blit(background,(0,0))
+    player.blit(player,(400,400))
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            exit()
+            # or pygame.quit()
+        if event.type == pygame.KEYDOWN:# checking if any key is pressed
+            if event.key == K_UP:
+                keys[0]=True
+            elif event.key == K_LEFT:
+                keys[1]=True
+            elif event.key == K_DOWN:
+                keys[2]=True
+            elif event.key == K_RIGHT:
+                keys[3]=True
+        #check if keyboard key is released
+        if event.type == pygame.KEYUP:
+            if event.key == K_UP:
+                keys[0]=False
+            elif event.key == K_LEFT:
+                keys[1]=False
+            elif event.key == K_DOWN:
+                keys[2]=False
+            elif event.key == K_RIGHT:
+                keys[3]=False
+    if keys[0]:
+        if player_y > 0:
+            player_y -= 10
+    elif keys[2]:
+        if player_y < 780:
+            player_y += 10
+
+    if keys[1]:
+        if player_x > 0:
+            player_x -= 10
+    elif keys[3]:
+        if player_x < 780:
+            player_x += 10
+
+
+    player_y+=10
+    sleep(0.5)
+print("Game Over")
